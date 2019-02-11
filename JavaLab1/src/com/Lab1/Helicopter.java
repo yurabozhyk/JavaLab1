0219948ad444b177b2f3633e189e7b12878d3881
Yura Bozhyk <yura.bozhyk@gmail.com>
2019-02-12 01:38:53 +0200

    Realization of class Helicopter with 7 fields, 8 methods and 3 constructors on Java

---
+++ JavaLab1/src/com/Lab1/Helicopter.java
@@ -0,0 +1,140 @@
+package com.Lab1;
+
+public class Helicopter {
+
+    static int numberBuilt = 0;
+
+    protected int price;
+    protected int yearBuilt;
+
+    private int maxLiftWeight;
+    private String name;
+    private int maxHeight;
+    private int passengerCapacity;
+    private int maxSpeedLimit;
+
+
+    public void setMaxLiftWeight(int maxLiftWeight) {
+        this.maxLiftWeight = maxLiftWeight;
+    }
+
+    public int getMaxLiftWeight() {
+        return maxLiftWeight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setMaxHeight(int maxHeight) {
+        this.maxHeight = maxHeight;
+    }
+
+    public int getMaxHeight() {
+        return maxHeight;
+    }
+
+    public void setPassengerCapacity(int passengerCapacity) {
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public void setMaxSpeedLimit(int maxSpeedLimit) {
+        this.maxSpeedLimit = maxSpeedLimit;
+    }
+
+    public int getMaxSpeedLimit() {
+        return maxSpeedLimit;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setYearBuilt(int yearBuilt) {
+        this.yearBuilt = yearBuilt;
+    }
+
+    public int getYearBuilt() {
+        return yearBuilt;
+    }
+
+//--------------------------------------------
+
+    public Helicopter() {
+        this(10, "Toy", 100, 1, 10, 1, 2019);
+    }
+
+    public Helicopter(int maxLiftWeight,
+                      String name,
+                      int maxHeight,
+                      int passengerCapacity) {
+
+        this(maxLiftWeight, name, maxHeight, passengerCapacity, 800, 15, 2000);
+    }
+
+    public Helicopter(int maxLiftWeight,
+                      String name,
+                      int maxHeight,
+                      int passengerCapacity,
+                      int maxSpeedLimit,
+                      int price,
+                      int yearBuilt) {
+
+        this.maxLiftWeight = maxLiftWeight;
+        this.name = name;
+        this.maxHeight = maxHeight;
+        this.passengerCapacity = passengerCapacity;
+        this.maxSpeedLimit = maxSpeedLimit;
+        this.price = price;
+        this.yearBuilt = yearBuilt;
+        numberBuilt++;
+    }
+
+    public String toString() {
+        return "\n\tHelicopter\n"
+                + "Max lifting weight: " + maxLiftWeight + " kg\n"
+                + "Name: " + name + "\n"
+                + "Max height: " + maxHeight + "m\n"
+                + "Passenger capacity: " + passengerCapacity + "\n"
+                + "Max speed limit: " + maxSpeedLimit + " km/h\n"
+                + "Price: " + price + "M$\n"
+                + "Year built: " + yearBuilt;
+    }
+
+    public static void printStaticNumberBuilt() {
+        System.out.println(numberBuilt + " helicopter built.");
+    }
+
+    public void printNumberBuilt() {
+        System.out.println(numberBuilt + " helicopter built.");
+    }
+
+    public void resetValues(int maxLiftWeight,
+                            String name,
+                            int maxHeight,
+                            int passengerCapacity,
+                            int maxSpeedLimit,
+                            int price,
+                            int yearBuilt) {
+
+        this.maxLiftWeight = maxLiftWeight;
+        this.name = name;
+        this.maxHeight = maxHeight;
+        this.passengerCapacity = passengerCapacity;
+        this.maxSpeedLimit = maxSpeedLimit;
+        this.price = price;
+        this.yearBuilt = yearBuilt;
+    }
+}
---
+++ JavaLab1/src/com/Lab1/Main.java
@@ -0,0 +1,17 @@
+package com.Lab1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Helicopter cargo = new Helicopter();
+        Helicopter civil = new Helicopter(2500, "HH-65", 15000, 4);
+        Helicopter military = new Helicopter(3000, "S-76C", 20000, 10, 600, 13, 1977);
+
+        System.out.println(cargo.toString());
+        System.out.println(civil.toString());
+        System.out.println(military.toString());
+
+        civil.printNumberBuilt();
+        Helicopter.printStaticNumberBuilt();
+    }
+}
